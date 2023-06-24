e4cd4c34e16821ce0507fee0990d430d57633834
abdula65 <manarovabdula0908@gmail.com>
2023-06-24 20:28:42 +0600

    'hd'

--- handlers/admin.py
+++ handlers/admin.py
@@ -86,13 +86,13 @@ async def del_callback_run(callback_query: types.CallbackQuery):
     await callback_query.answer(text=f'{callback_query.data.replace("del ", "")} Удалена', show_alert=True)
 
 
-#@dp.message_handler(commands='Удалить')
+# @dp.message_handler(commands='Удалить')
 async def delete_item(message: types.Message):
     if message.from_user.id == ID:
         read = await sqlite_dp.sql_read2()
         for ret in read:
             await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена {ret[-1]}')
-            await bot.send_message(message.from_user.id, text='^^^', reply_markup=InlineKeyboardMarkup().\
+            await bot.send_message(message.from_user.id, text='^^^', reply_markup=InlineKeyboardMarkup(). \
                                    add(InlineKeyboardButton(f'Удалить {ret[1]}', callback_data=f'del {ret[1]}')))
 
 
@@ -106,4 +106,3 @@ def register_handler_admin(dp: Dispatcher):
     dp.register_message_handler(load_price, state=FSMAdmin.price)
     dp.register_message_handler(cancel_handler, state="*", commands='отмена')
     dp.register_message_handler(delete_item, commands='Удалить')
-
